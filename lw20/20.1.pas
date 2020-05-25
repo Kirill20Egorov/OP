7ec8d234b1d7e8b393c5362ddedeeec8453d8dfd
Kirill20Egorov <kirill1337egorov2000@gmail.com>
2020-05-25 12:11:45 +0300

    FIXED

--- lw20/20.1.pas
+++ lw20/20.1.pas
@@ -4,6 +4,7 @@ CONST
   MaxPosition = 25;
   LineSize = 5;
   SymbolForPrint = 'X';
+  SymbolSpace = ' ';
   SymbolA = [3, 7, 9, 11, 12, 13, 14, 15, 16, 20];
   SymbolC = [1, 2, 3, 4, 5, 6, 11, 16, 21, 22, 23, 24, 25];
   SymbolE = [1, 2, 3, 4, 5, 6, 11, 12, 13, 14, 15, 16, 21, 22, 23, 24, 25]; 
@@ -24,49 +25,57 @@ TYPE
   Sieve = SET OF MinPosition..MaxPosition;
 VAR
   Ch: CHAR;
-PROCEDURE PrintSymbol(CombinationOfSymbol: Sieve);
+  CorrectSymbol: BOOLEAN;
+PROCEDURE PrintSymbol(CombinationOfSymbol: Sieve; VAR CorrectSymbol: BOOLEAN);
 VAR
   Count: INTEGER;
 BEGIN
-  FOR Count := MinPosition TO MaxPosition
-  DO
-    BEGIN         
-      IF Count IN CombinationOfSymbol
-      THEN
-        WRITE(SymbolForPrint)
-      ELSE
-        WRITE(' ');
-      IF (Count MOD LineSize = 0)
-      THEN
-        WRITELN
-    END
+  IF CorrectSymbol = TRUE
+  THEN
+    FOR Count := MinPosition TO MaxPosition
+    DO
+      BEGIN         
+        IF Count IN CombinationOfSymbol
+        THEN
+          WRITE(SymbolForPrint)
+        ELSE
+          WRITE(SymbolSpace);
+        IF (Count MOD LineSize = 0)
+        THEN
+          WRITELN
+      END
 END;
-PROCEDURE FindSymbol(VAR Ch: CHAR);
+FUNCTION FindSymbol(Ch: CHAR; VAR CorrectSymbol: BOOLEAN): Sieve;
 BEGIN
+  CorrectSymbol := TRUE;
   IF NOT EOLN(INPUT)
   THEN
     READ(INPUT, Ch);   
   CASE Ch OF
-    'A': PrintSymbol(SymbolA);
-    'C': PrintSymbol(SymbolC);
-    'E': PrintSymbol(SymbolE);
-    'F': PrintSymbol(SymbolF);
-    'H': PrintSymbol(SymbolH);
-    'I': PrintSymbol(SymbolI);
-    'L': PrintSymbol(SymbolL);  
-    'M': PrintSymbol(SymbolM);    
-    'N': PrintSymbol(SymbolN);
-    'O': PrintSymbol(SymbolO);  
-    'P': PrintSymbol(SymbolP);
-    'S': PrintSymbol(SymbolS);
-    'T': PrintSymbol(SymbolT);
-    'U': PrintSymbol(SymbolU);
-    'Y': PrintSymbol(SymbolY);
-    'Z': PrintSymbol(SymbolZ)                 
-    ELSE WRITELN('COULDN''T FIND CHARACTER') 
+    'A': FindSymbol := SymbolA; 
+    'C': FindSymbol := SymbolC;
+    'E': FindSymbol := SymbolE;
+    'F': FindSymbol := SymbolF;
+    'H': FindSymbol := SymbolH;
+    'I': FindSymbol := SymbolI;
+    'L': FindSymbol := SymbolL;  
+    'M': FindSymbol := SymbolM;    
+    'N': FindSymbol := SymbolN;
+    'O': FindSymbol := SymbolO;  
+    'P': FindSymbol := SymbolP;
+    'S': FindSymbol := SymbolS;
+    'T': FindSymbol := SymbolT;
+    'U': FindSymbol := SymbolU;
+    'Y': FindSymbol := SymbolY;
+    'Z': FindSymbol := SymbolZ               
+    ELSE 
+      BEGIN
+        WRITELN('COULDN''T FIND CHARACTER');
+        CorrectSymbol := FALSE
+      END 
   END
 END;
 BEGIN
   WRITELN('WRITE ONLY ONE OF THE FOLLOWING UPPERCASE CHARACTER: A, C, E, F, H, I, L, M, N, O, P, S, T, U, Y, Z'); 
-  FindSymbol(Ch)
+  PrintSymbol(FindSymbol(Ch, CorrectSymbol), CorrectSymbol)
 END.
